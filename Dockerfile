921da3389d1612512461eacfdbf9edf3f0e8bf57
snowdream <sn0wdr1am@qq.com>
2024-06-15 23:56:16 +0800

    feat: nginx

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
-FROM alpine:3.20.0
+FROM snowdreamtech/alpine:3.20.0
 
 LABEL maintainer="snowdream <sn0wdr1am@qq.com>"
 
-RUN apk add --no-cache musl-locales \
-    musl-locales-lang \
-    tzdata
+RUN apk add --no-cache nginx
+
+ENTRYPOINT ["/usr/sbin/nginx","-g", "daemon off;"]
+
+CMD ["-c", "/etc/nginx/nginx.conf"]
+
--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-DOCKER_HUB_PROJECT=snowdreamtech/alpine
+DOCKER_HUB_PROJECT=snowdreamtech/nginx
 
-GITHUB_PROJECT=ghcr.io/snowdreamtech/alpine
+GITHUB_PROJECT=ghcr.io/snowdreamtech/nginx
 
 docker buildx build --platform=linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/ppc64le,linux/riscv64,linux/s390x \
 -t ${DOCKER_HUB_PROJECT}:latest \
--t ${DOCKER_HUB_PROJECT}:3.20.0 \
--t ${DOCKER_HUB_PROJECT}:3.20 \
--t ${DOCKER_HUB_PROJECT}:3 \
+-t ${DOCKER_HUB_PROJECT}:1.26.1 \
+-t ${DOCKER_HUB_PROJECT}:1.26\
+-t ${DOCKER_HUB_PROJECT}:1 \
 -t ${GITHUB_PROJECT}:latest \
--t ${GITHUB_PROJECT}:3.20.0 \
--t ${GITHUB_PROJECT}:3.20 \
--t ${GITHUB_PROJECT}:3 \
+-t ${GITHUB_PROJECT}:1.26.1 \
+-t ${GITHUB_PROJECT}:1.26 \
+-t ${GITHUB_PROJECT}:1 \
 . \
 --push
